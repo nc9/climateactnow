812f00b0b94e13f4b23794e116fd43b96c5ca2a6
Jack Nutt <wp381640@protonmail.com>
2020-02-27 07:36:47 +0000

    first build

---
+++ docs/precache-manifest.dba76e024e5eae83709fbc7ee67aa94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8889017f5da3b56e1c3529d4dfb76f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb15d25a0a04da1e5a8f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "77b20aecde4cc706853f",
+    "url": "/static/js/2.df3b8de4.chunk.js"
+  },
+  {
+    "revision": "cc11d447fea95d82b419d88027307c45",
+    "url": "/static/js/2.df3b8de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb15d25a0a04da1e5a8f",
+    "url": "/static/js/main.a4004eba.chunk.js"
+  },
+  {
+    "revision": "0a93640c6fe0293b2b0c",
+    "url": "/static/js/runtime-main.24b56609.js"
+  }
+]);
