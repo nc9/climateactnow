69fa8effebc05bc9f0362e2775624ac48af8e4b5
Jack Nutt <wp381640@protonmail.com>
2020-02-27 07:39:32 +0000

    fix build subdirectory

---
+++ docs/precache-manifest.6f9bdf8deb89654e2259c1f40cc0bfb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af083a630fd4956da59a1c38764726b",
+    "url": "/climatevotedata/index.html"
+  },
+  {
+    "revision": "cb15d25a0a04da1e5a8f",
+    "url": "/climatevotedata/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "77b20aecde4cc706853f",
+    "url": "/climatevotedata/static/js/2.df3b8de4.chunk.js"
+  },
+  {
+    "revision": "cc11d447fea95d82b419d88027307c45",
+    "url": "/climatevotedata/static/js/2.df3b8de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb15d25a0a04da1e5a8f",
+    "url": "/climatevotedata/static/js/main.a4004eba.chunk.js"
+  },
+  {
+    "revision": "6d5bafb39033f345abde",
+    "url": "/climatevotedata/static/js/runtime-main.a1d33def.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dba76e024e5eae83709fbc7ee67aa94a.js"
+  "/climatevotedata/precache-manifest.6f9bdf8deb89654e2259c1f40cc0bfb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/climatevotedata/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
